9c529504c7b5a3f90b438e61c8d2a5f7bc70da92
sarangyoloh <sarang.k@yoloh.com>
2024-10-11 09:51:59 +0530

    added the update-code function

--- script.py
+++ script.py
@@ -24,6 +24,7 @@ cli_commands = []
 apply_json = {}
 lambda_config_json = {}
 build_proj_names = {}
+is_parent_built = False
 
 def parse_json(file_path):
     with open(file_path, 'r') as file:
@@ -36,10 +37,22 @@ def parse_json(file_path):
 
 def apply_file_reader(content):
     create_function_reader(content["create_function"])
+    update_function_code_reader(content["update_function_code"])
+
+def update_function_code_reader(update_function_code):
+    if not is_parent_built:
+        install_parent_build()
+        is_parent_built = True
+
+    for logical_id in update_function_code:
+        aws_cli_update_code_cmd_gen(get_lambda_config_detail(logical_id))
 
 def create_function_reader(create_function):
+    install_parent_build()
+    is_parent_built = True
+    
     for logical_id in create_function:
-        aws_cli_create_function_command_gen(get_lambda_config_detail(logical_id))
+        aws_cli_create_function_cmd_gen(get_lambda_config_detail(logical_id))
         
 def get_lambda_config_detail(config_name):
     if is_logical_id_present(config_name):
@@ -48,23 +61,48 @@ def get_lambda_config_detail(config_name):
 def is_logical_id_present(config_name):
     return lambda_config_json.get(config_name)
 
-def aws_cli_create_function_command_gen(lambda_config_detail):
+def aws_cli_update_code_cmd_gen(lambda_config_detail):
+    update_code_cmd = {}
+    cmd = update_function_code_cmd
+    project_dir = lambda_config_detail.get("project_dir")
+    if(project_dir not in build_proj_names):
+        jar_path = package_child_build(project_dir)
+        update_code_cmd['description'] = f"deploying the jar present in path: {jar_path}"
+        update_code_cmd['cmd'] = f"aws s3 cp {jar_path} s3://<Bucket>/<Key>"
+        cli_commands.append(update_code_cmd.copy())
+        build_proj_names[f"{project_dir}"] = jar_path
+        
+    jar_name = build_proj_names.get(project_dir).split("/")[-1]
+    cmd = argument_appender(cmd, "function-name", lambda_config_detail.get("function-name"))
+    cmd = argument_appender(cmd, "s3-bucket", lambda_config_detail.get("function-name"))
+    cmd = argument_appender(cmd, "s3-key", jar_name)
+
+    update_code_cmd['description'] = f"updating the function code for {lambda_config_detail.get("function-name")}"
+    update_code_cmd['cmd'] = cmd
+    cli_commands.append(update_code_cmd)
+
+def aws_cli_create_function_cmd_gen(lambda_config_detail):
     create_cmd = {}
-    install_parent_build()
     cmd = create_function_cmd
     for config_key in lambda_config_detail:
         if(config_key == "project_dir"):
             project_dir = lambda_config_detail.get("project_dir")
-            if (project_dir not in build_proj_names):
+            
+            if(project_dir not in build_proj_names):
                 jar_path = package_child_build(project_dir)
-                s3_cmd = {}
-                s3_cmd['description'] = f"deploying the jar present in path: {jar_path}"
-                s3_cmd['cmd'] = f"aws s3 cp {jar_path} s3://<Bucket>/<Key>"
-                cli_commands.append(s3_cmd)
+                create_cmd['description'] = f"deploying the jar present in path: {jar_path}"
+                create_cmd['cmd'] = f"aws s3 cp {jar_path} s3://<Bucket>/<Key>"
+                cli_commands.append(create_cmd.copy())
                 build_proj_names[f"{project_dir}"] = jar_path
-        config_val = lambda_config_detail.get(config_key)
+            
+            jar_name = build_proj_names.get(project_dir).split("/")[-1]
+            cmd = comma_sep_argument_appender(cmd, "code", {'S3Bucket': "name", "S3Key": jar_name})         
+        
+        config_val = lambda_config_detail.get(config_key)    
+        
         if config_key != "function-alias" and config_key != "function-version" and config_key != "project_dir":
             cmd = argument_appender(cmd, config_key, config_val)     
+    
     create_cmd['description'] = f"create the {lambda_config_detail['function-name']}"
     create_cmd['cmd'] = cmd
     cli_commands.append(create_cmd)
@@ -83,10 +121,17 @@ def package_child_build(project_dir):
            result = full_path
            break
     return result        
-    
+
+# S3Bucket=<your-bucket-name>,S3Key=<your-lambda-code>.jar
 
 def argument_appender(cmd, config_key, config_val):
     return cmd + f' --{config_key} {config_val}'
+
+def comma_sep_argument_appender(cmd, config_key, args):
+    cmd = cmd + f" =={config_key} "
+    for key, val in args.items():
+        cmd = cmd + key + "=" + val + ","
+    return cmd[:-1]
     
 if os.path.exists(apply_path) and os.path.exists(lambda_config_path):
     print("Both the files apply.json and lambda_config.json found")
